159febbe6e8685f9566dd0b6275575d2a406e5f6
Carter Green <cartergr@usc.edu>
2017-10-26 13:47:06 -0700

    Additional custom scripts

--- polybar/spotify.sh
+++ polybar/spotify.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-
-if [ "$(playerctl status)" = "Playing" ]; then
+# Capture both stderr and stdout
+status=$(playerctl status 2>&1)
+if [[ "$status" == "Playing" ]]; then
     title=`exec playerctl metadata xesam:title`
     artist=`exec playerctl metadata xesam:artist`
     echo "$title | $artist"
