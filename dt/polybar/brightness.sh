bc1f26c91e358135863a604f3b9ba078b7c6cb9f
Carter Green <cgreen@belvederetrading.com>
2018-12-07 10:15:13 -0600

    Remove irrelevant files

--- dt/polybar/brightness.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-# Get current backlight percentage
-brightness=`light -G | cut -d '.' -f 1`
-echo "$brightness%"
--- lt/polybar/updates.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Ignore stderr
-exec 2> /dev/null
-if [ "$1" == "aur" ]; then
-    checkupdates-aur | wc -l
-else
-    checkupdates | wc -l
-fi
