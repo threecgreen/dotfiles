e204eecd66f2a8ad99c2f8c3bb6458a67cda479b
Carter Green <cartergr@usc.edu>
2020-09-22 12:29:20 -0500

    Handle expressvpn daemon stopped

--- polybar/expressvpn.sh
+++ polybar/expressvpn.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/bash
+# Check if non-zero exit code
+expressvpn status > /dev/null 2> /dev/null
+if [[ "$?" == "1" ]]; then
+    echo "daemon stopped"
+    return
+fi
 # Store first word in first line and remove character formatting
 fw=$(expressvpn status | head -n 1 | cut -d' ' -f 1 | sed -r 's/\x1B\[[0-9;]+m//g')
 # Not connected
