0a43022800b3bb97d8d7392d081b818edd53e8c8
Carter Green <cartergr@usc.edu>
2021-02-21 18:26:59 -0600

    Fix spotify and playerctl

--- polybar/spotify.sh
+++ polybar/spotify.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 # Capture both stderr and stdout
-status=$(playerctl status 2>&1)
+status=$(playerctl status -p spotify 2>&1)
 if [[ "$status" == "Playing" ]]; then
-    title=`exec playerctl metadata xesam:title`
-    artist=`exec playerctl metadata xesam:artist`
+    title=`exec playerctl -p spotify metadata xesam:title`
+    artist=`exec playerctl -p spotify metadata xesam:artist`
     echo "$title | $artist"
 else
     echo ""
