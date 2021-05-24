eee8918f73ec62bd33b63a2f19d257f36fe2666c
Carter Green <cartergr@usc.edu>
2021-05-24 08:48:18 -0500

    Audio sink control

---
+++ polybar/default-sink.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+default_sink="$(pactl info | sed -En 's/Default Sink: (.*)/\1/p')"
+case "$default_sink"  in
+    *USB* ) echo -e "speakers \UE32D";;
+    *4.analog-stereo ) echo "headphones ";;
+    * ) echo "unknown";;
+esac
+
