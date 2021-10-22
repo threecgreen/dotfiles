9ea31f607c641fe8db2cf37a98fd7956d20c7fbf
Carter Green <cartergr@usc.edu>
2021-10-21 21:31:21 -0500

    Fix i3lock script

--- i3/lock.sh
+++ i3/lock.sh
@@ -2,10 +2,18 @@
 # For i3lock-color-git not i3lock
 # --textcolor=002b36cc 
 i3lock -f --blur=12 -k --indicator \
-    --insidecolor=83949688 --line-uses-inside --ringcolor=002b3688 \
-    --keyhlcolor=2aa19888 --bshlcolor=dc322f88 --separatorcolor=eee8d588 \
-    --insidevercolor=2aa19888 --insidewrongcolor=cb4b1688 \
-    --ringvercolor=2aa19888 --ringwrongcolor=cb4b1688 \
-    --timecolor=002b36cc --datecolor=002b36cc \
-    --timestr="%H:%M" \
+    --inside-color=83949688 \
+    --line-uses-inside \
+    --ring-color=002b3688 \
+    --keyhl-color=2aa19888 \
+    --bshl-color=dc322f88 \
+    --separator-color=eee8d588 \
+    --insidever-color=2aa19888 \
+    --insidewrong-color=cb4b1688 \
+    --ringver-color=2aa19888 \
+    --ringwrong-color=cb4b1688 \
+    --time-color=002b36cc \
+    --date-color=002b36cc \
+    --time-str="%H:%M" \
+    --date-str="%B %e, %Y" \
     --ring-width=20
