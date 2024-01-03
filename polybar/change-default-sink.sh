b129696bc8171dd77936a2ef4c9e10fc72f0fe38
Carter Green <cartergr@usc.edu>
2024-01-03 08:52:17 -0600

    Update network interfaces and audio sinks

--- polybar/change-default-sink.sh
+++ polybar/change-default-sink.sh
@@ -2,6 +2,6 @@
 default_sink="$(pactl info | sed -En 's/Default Sink: (.*)/\1/p')"
 case "$default_sink" in
     *USB* ) pactl set-default-sink alsa_output.pci-0000_0f_00.4.analog-stereo;;
-    *4.analog-stereo ) pactl set-default-sink alsa_output.usb-Burr-Brown_from_TI_USB_Audio_DAC-00.analog-stereo;;
+    *4.analog-stereo ) pactl set-default-sink alsa_output.usb-Burr-Brown_from_TI_USB_Audio_DAC-00.iec958-stereo;;
     * ) ;;
 esac
